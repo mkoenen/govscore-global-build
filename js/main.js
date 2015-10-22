1db77505be5ba6d2bc73835ccb6c48103094245f
Margarete Koenen <margarete.koenen@loudfeed.com>
2015-10-22 15:25:21 -0400

    amplify push code

--- js/main.js
+++ js/main.js
@@ -1060,7 +1060,38 @@ function calcResults() {
 
 
 /* Pushwoosh ---------------------------------------------------*/
-function initPushwoosh() {
+
+function registerPushwooshAndroid() {
+    var pushNotification = cordova.require("com.pushwoosh.plugins.pushwoosh.PushNotification");
+ 
+    //set push notifications handler
+    document.addEventListener('push-notification', function(event) {
+        var title = event.notification.title;
+        var userData = event.notification.userdata;
+                                 
+        if(typeof(userData) != "undefined") {
+            console.warn('user data: ' + JSON.stringify(userData));
+        }
+                                     
+        alert(title);
+    });
+ 
+    //initialize Pushwoosh with projectid: "GOOGLE_PROJECT_ID", pw_appid : "PUSHWOOSH_APP_ID". This will trigger all pending push notifications on start.
+    pushNotification.onDeviceReady({ projectid: "335672732764", pw_appid : "42686-7A1E3" });
+ 
+    //register for pushes
+    pushNotification.registerDevice(
+        function(status) {
+            var pushToken = status;
+            console.warn('push token: ' + pushToken);
+        },
+        function(status) {
+            console.warn(JSON.stringify(['failed to register ', status]));
+        }
+    );
+}
+
+function registerPushwooshIOS() {
     var pushNotification = cordova.require("com.pushwoosh.plugins.pushwoosh.PushNotification");
  
     //set push notification callback before we initialize the plugin
@@ -1086,74 +1117,10 @@ function initPushwoosh() {
         },
         function(status) {
             console.warn('failed to register : ' + JSON.stringify(status));
-            alert(JSON.stringify(['failed to register ', status]));
+            //alert(JSON.stringify(['failed to register ', status]));
         }
     );
      
     //reset badges on app start
     pushNotification.setApplicationIconBadgeNumber(0);
 }
-
-// function registerPushwooshAndroid() {
-//     var pushNotification = window.plugins.pushNotification;
- 
-//     //set push notifications handler
-//     document.addEventListener('push-notification', function(event) {
-//         var title = event.notification.title;
-//         var userData = event.notification.userdata;
-                                 
-//         if(typeof(userData) != "undefined") {
-//             console.warn('user data: ' + JSON.stringify(userData));
-//         }
-                                     
-//         alert(title);
-//     });
- 
-//     //initialize Pushwoosh with projectid: "GOOGLE_PROJECT_ID", pw_appid : "PUSHWOOSH_APP_ID". This will trigger all pending push notifications on start.
-//     pushNotification.onDeviceReady({ projectid: "335672732764", pw_appid : "42686-7A1E3" });
- 
-//     //register for pushes
-//     pushNotification.registerDevice(
-//         function(status) {
-//             var pushToken = status;
-//             console.warn('push token: ' + pushToken);
-//         },
-//         function(status) {
-//             console.warn(JSON.stringify(['failed to register ', status]));
-//         }
-//     );
-// }
-
-// function registerPushwooshIOS() {
-//     var pushNotification = window.plugins.pushNotification;
- 
-//     //set push notification callback before we initialize the plugin
-//     document.addEventListener('push-notification', function(event) {
-//                                 //get the notification payload
-//                                 var notification = event.notification;
- 
-//                                 //display alert to the user for example
-//                                 alert(notification.aps.alert);
-                               
-//                                 //clear the app badge
-//                                 pushNotification.setApplicationIconBadgeNumber(0);
-//                             });
- 
-//     //initialize the plugin
-//     pushNotification.onDeviceReady({pw_appid:"42686-7A1E3"});
-     
-//     //register for pushes
-//     pushNotification.registerDevice(
-//         function(status) {
-//             var deviceToken = status['deviceToken'];
-//             console.warn('registerDevice: ' + deviceToken);
-//         },
-//         function(status) {
-//             console.warn('failed to register : ' + JSON.stringify(status));
-//             //alert(JSON.stringify(['failed to register ', status]));
-//         }
-//     );
-     
-//     //reset badges on app start
-//     pushNotification.setApplicationIconBadgeNumber(0);
-// }
